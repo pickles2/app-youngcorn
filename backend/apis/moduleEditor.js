e72192884fe01a4654b2c3918de4d63d42c5fb3c
Tomoya Koyanagi <tomk79@gmail.com>
2015-09-17 17:31:02 +0900

    moduleEditor: モジュールコードの編集と保存ができるようになった。

--- backend/apis/moduleEditor.js
+++ backend/apis/moduleEditor.js
@@ -10,64 +10,64 @@ module.exports = function( data, callback, main, socket ){
 	var rtn = {},
 		px2proj;
 
-	if( data.fnc == 'loadSrc' ){
-		new Promise(function(rlv){rlv();})
-			.then(function(){ return new Promise(function(rlv, rjt){
-				// プロジェクト情報を取得する
-				main.px2dtLDA.getProject(
-					data.projectIdx,
-					function( projectInfo ){
-						rtn.projectInfo = projectInfo;
-						rlv();
-					}
-				);
-			}); })
-			.then(function(){ return new Promise(function(rlv, rjt){
-				// px2projオブジェクトを生成する
-				main.getPx2Proj( rtn.projectInfo, function(_px2proj){
-					px2proj = _px2proj;
+	new Promise(function(rlv){rlv();})
+		.then(function(){ return new Promise(function(rlv, rjt){
+			// プロジェクト情報を取得する
+			main.px2dtLDA.getProject(
+				data.projectIdx,
+				function( projectInfo ){
+					rtn.projectInfo = projectInfo;
 					rlv();
-				} );
-			}); })
-			.then(function(){ return new Promise(function(rlv, rjt){
-				// Px2のコンフィグを取得する
-				px2proj.get_config(function(config){
-					// console.log('message: '+ JSON.stringify(conf));
-					rtn.config = config;
+				}
+			);
+		}); })
+		.then(function(){ return new Promise(function(rlv, rjt){
+			// px2projオブジェクトを生成する
+			main.getPx2Proj( rtn.projectInfo, function(_px2proj){
+				px2proj = _px2proj;
+				rlv();
+			} );
+		}); })
+		.then(function(){ return new Promise(function(rlv, rjt){
+			// Px2のコンフィグを取得する
+			px2proj.get_config(function(config){
+				// console.log('message: '+ JSON.stringify(conf));
+				rtn.config = config;
 
-					if( !rtn.config ){
-						rtn.config = false;
-						rjt();
-						return;
-					}
-					rlv();
-				});
-			}); })
-			.then(function(){ return new Promise(function(rlv, rjt){
-				// パッケージ情報を取得
-				data.moduleId.match( new RegExp('^([\\s\\S]*)\\:([\\s\\S]*)\\/([\\s\\S]*)$') );
-				rtn.packageId = RegExp.$1;
-				rtn.categoryId = RegExp.$2;
-				rtn.moduleId = RegExp.$3;
-				rtn.packageId = data.packageId;
-				try {
-					rtn.packageRealpath = rtn.config.plugins.px2dt.paths_module_template[data.packageId];
-					rtn.packageRealpath = path.resolve( php.dirname( rtn.projectInfo.path + '/' + rtn.projectInfo.entry_script ), rtn.packageRealpath )+'/';
-				} catch (e) {
-					rtn.packageId = false;
-					rtn.packageRealpath = false;
+				if( !rtn.config ){
+					rtn.config = false;
 					rjt();
 					return;
 				}
-				try {
-					rtn.packageInfo = require( rtn.packageRealpath + 'info.json' );
-				} catch (e) {
-					rtn.packageInfo = false;
-				}
-				rtn.moduleRealpath = path.resolve( rtn.packageRealpath, rtn.categoryId, rtn.moduleId );
 				rlv();
-			}); })
-			.then(function(){ return new Promise(function(rlv, rjt){
+			});
+		}); })
+		.then(function(){ return new Promise(function(rlv, rjt){
+			// パッケージ情報を取得
+			data.moduleId.match( new RegExp('^([\\s\\S]*)\\:([\\s\\S]*)\\/([\\s\\S]*)$') );
+			rtn.packageId = RegExp.$1;
+			rtn.categoryId = RegExp.$2;
+			rtn.moduleId = RegExp.$3;
+			rtn.packageId = data.packageId;
+			try {
+				rtn.packageRealpath = rtn.config.plugins.px2dt.paths_module_template[data.packageId];
+				rtn.packageRealpath = path.resolve( php.dirname( rtn.projectInfo.path + '/' + rtn.projectInfo.entry_script ), rtn.packageRealpath )+'/';
+			} catch (e) {
+				rtn.packageId = false;
+				rtn.packageRealpath = false;
+				rjt();
+				return;
+			}
+			try {
+				rtn.packageInfo = require( rtn.packageRealpath + 'info.json' );
+			} catch (e) {
+				rtn.packageInfo = false;
+			}
+			rtn.moduleRealpath = path.resolve( rtn.packageRealpath, rtn.categoryId, rtn.moduleId );
+			rlv();
+		}); })
+		.then(function(){ return new Promise(function(rlv, rjt){
+			if( data.fnc == 'loadSrc' ){
 				// モジュールコードを取得
 				rtn.src = {};
 				function loadFile( filename ){
@@ -90,17 +90,27 @@ module.exports = function( data, callback, main, socket ){
 					rtn.src.css = loadFile('module.css');
 				}
 				rtn.src.js = loadFile('module.js');
+
+				rtn.result = true;
 				rlv();
-			}); })
-			.then(function(){ return new Promise(function(rlv, rjt){
-				// 返却
-				callback(rtn);
-				rlv();
-			}); })
-		;
-	}else{
-		callback(false);
-	}
+
+			}else if( data.fnc == 'saveSrc' ){
+				// モジュールコードを保存
+				var filepath = path.resolve(rtn.moduleRealpath, data.filename);
+				fs.writeFile( filepath, data.src, function(err){
+					rtn.result = !err;
+					rlv();
+				} );
+			}else{
+				rjt();return;
+			}
+		}); })
+		.then(function(){ return new Promise(function(rlv, rjt){
+			// 返却
+			callback(rtn);
+			rlv();
+		}); })
+	;
 
 	return;
 }
--- src/project/moduleEditor/editor/index_files/cont.js
+++ src/project/moduleEditor/editor/index_files/cont.js
@@ -14,7 +14,7 @@ window.cont = new (function(){
 					data.projectIdx = php.intval($.url(window.location.href).param('projectIdx'));
 					data.packageId = php.trim($.url(window.location.href).param('packageId'));
 					data.moduleId = php.trim($.url(window.location.href).param('moduleId'));
-					console.log( data );
+					// console.log( data );
 					it1.next(data);
 				} ,
 				function(it1, data){
@@ -27,7 +27,8 @@ window.cont = new (function(){
 							'moduleId': data.moduleId
 						},
 						function(result){
-							console.log( result );
+							// console.log( result );
+							data.src = result.src;
 							// data.projectInfo = result.projectInfo;
 							// data.config = result.config;
 							// data.path_homedir = result.path_homedir;
@@ -37,26 +38,121 @@ window.cont = new (function(){
 				} ,
 				function(it1, data){
 					// 描画
-					// var html =
-					// 	twig({data: document.getElementById('template-moduleList').innerHTML})
-					// 	.render(data)
-					// ;
-					// document.querySelector('.cont_module_list').innerHTML = html;
-					//
-					// var html =
-					// 	twig({data: document.getElementById('template-content-footer').innerHTML})
-					// 	.render(data)
-					// ;
-					// document.querySelector('.cont_cont_footer').innerHTML = html;
+
+					// info.json
+					if( data.src.infoJson ){
+						$('[name=src_infojson]')
+							.val( data.src.infoJson.src )
+							.attr( {"data-filename": data.src.infoJson.filename} )
+						;
+					}else{
+						$('[name=src_infojson]')
+							.val( JSON.stringify( {
+								"name": ""
+							}, null, 2 ) )
+							.attr( {"data-filename": 'info.json'} )
+						;
+					}
+
+					// template.html
+					if( data.src.html ){
+						$('[name=src_html]')
+							.val( data.src.html.src )
+							.attr( {"data-filename": data.src.html.filename} )
+						;
+					}else{
+						$('[name=src_html]')
+							.val( '<div></div>' )
+							.attr( {"data-filename": 'template.html'} )
+						;
+					}
+
+					// module.css.scss
+					if( data.src.css ){
+						$('[name=src_css]')
+							.val( data.src.css.src )
+							.attr( {"data-filename": data.src.css.filename} )
+						;
+					}else{
+						$('[name=src_css]')
+							.val( '@charset "utf-8";' )
+							.attr( {"data-filename": 'module.css.scss'} )
+						;
+					}
+
+					// module.js
+					if( data.src.js ){
+						$('[name=src_js]')
+							.val( data.src.js.src )
+							.attr( {"data-filename": data.src.js.filename} )
+						;
+					}else{
+						$('[name=src_js]')
+							.val( '' )
+							.attr( {"data-filename": 'module.js'} )
+						;
+					}
+
+					_this.updatePreview(function(){
+						it1.next(data);
+					});
+
+				} ,
+				function(it1, data){
+					// イベント処理
+
+					$('textarea').change(function(e){
+						console.log('changed');
+						var $this = $(this);
+						console.log($this.attr('data-filename'));
+
+						main.socket.send('moduleEditor',
+							{
+								'fnc': 'saveSrc' ,
+								'filename': $this.attr('data-filename') ,
+								'src': $this.val(),
+								'projectIdx': data.projectIdx ,
+								'packageId': data.packageId ,
+								'moduleId': data.moduleId
+							},
+							function(result){
+								console.log( result );
+								alert( '保存しました。' );
+								_this.updatePreview( function(){} );
+							}
+						);
+
+					});
+
 
 					it1.next(data);
 				} ,
 				function(it1, _data){
 					data = _data;
+					console.log(data);
 					console.log('Started!');
 				}
 			]);
 		});
+		return this;
+	}
+
+	/**
+	 * プレビューを更新する。
+	 * @param  {Function} callback コールバック関数
+	 * @return {Object}            this
+	 */
+	this.updatePreview = function(callback){
+		callback = callback||function(){};
+		var $iframe = $($('iframe.cont_preview').get(0).contentWindow);
+		var doc = $iframe.get(0).document;
+		doc.write('<html><body><p>aaaaa</p></body></html>');
+		doc.close();
+
+		setTimeout(function(){
+			callback();
+		}, 0);
+		return this;
 	}
 
 })();

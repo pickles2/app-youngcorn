1323ac3734ae275ea837ea4c416aff2e7f46520f
Tomoya Koyanagi <tomk79@gmail.com>
2015-11-12 18:32:15 +0900

    「サイトマップフォルダを開く」を追加

--- dist/project/sitemapEditor/index_files/cont.js
+++ dist/project/sitemapEditor/index_files/cont.js
@@ -13350,6 +13350,7 @@ window.cont = new (function(){
 	var _this = this;
 	var it79 = require('iterate79');
 	var php = require('phpjs');
+	var data = {};
 
 	var projectIdx;
 
@@ -13358,7 +13359,7 @@ window.cont = new (function(){
 		 * initialize
 		 */
 		main.init(function(){
-			it79.fnc({}, [
+			it79.fnc(data, [
 				function(it1, data){
 					console.log('setup env...');
 					setTimeout(function(){
@@ -13366,10 +13367,40 @@ window.cont = new (function(){
 					}, 10);
 				},
 				function(it1, data){
+					data.projectIdx = php.intval($.url(window.location.href).param('projectIdx'));
+					it1.next(data);
+				},
+				function(it1, data){
+					// プロジェクト情報を取得
+					main.socket.send('getProject', {'projectIdx': data.projectIdx}, function(result){
+						// console.log( result );
+						data.projectInfo = result.projectInfo;
+						data.config = result.config;
+						data.path_homedir = result.path_homedir;
+						it1.next(data);
+					});
+				},
+				function(it1, data){
 					console.log('Started!');
 				}
 			]);
 		});
+	}
+
+	/**
+	 * サイトマップフォルダを開く
+	 * @return {[type]} [description]
+	 */
+	this.openSitemapFolder = function(){
+		main.socket.send(
+			'open' ,
+			{
+				'path': data.path_homedir+'/sitemaps/'
+			} ,
+			function(result){
+			}
+		);
+		return this;
 	}
 
 })();
--- src/project/sitemapEditor/index_files/cont.js
+++ src/project/sitemapEditor/index_files/cont.js
@@ -2,6 +2,7 @@ window.cont = new (function(){
 	var _this = this;
 	var it79 = require('iterate79');
 	var php = require('phpjs');
+	var data = {};
 
 	var projectIdx;
 
@@ -10,7 +11,7 @@ window.cont = new (function(){
 		 * initialize
 		 */
 		main.init(function(){
-			it79.fnc({}, [
+			it79.fnc(data, [
 				function(it1, data){
 					console.log('setup env...');
 					setTimeout(function(){
@@ -18,10 +19,40 @@ window.cont = new (function(){
 					}, 10);
 				},
 				function(it1, data){
+					data.projectIdx = php.intval($.url(window.location.href).param('projectIdx'));
+					it1.next(data);
+				},
+				function(it1, data){
+					// プロジェクト情報を取得
+					main.socket.send('getProject', {'projectIdx': data.projectIdx}, function(result){
+						// console.log( result );
+						data.projectInfo = result.projectInfo;
+						data.config = result.config;
+						data.path_homedir = result.path_homedir;
+						it1.next(data);
+					});
+				},
+				function(it1, data){
 					console.log('Started!');
 				}
 			]);
 		});
+	}
+
+	/**
+	 * サイトマップフォルダを開く
+	 * @return {[type]} [description]
+	 */
+	this.openSitemapFolder = function(){
+		main.socket.send(
+			'open' ,
+			{
+				'path': data.path_homedir+'/sitemaps/'
+			} ,
+			function(result){
+			}
+		);
+		return this;
 	}
 
 })();
